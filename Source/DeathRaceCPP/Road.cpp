538bca9863003248a8ea1f4014c09df4e574a38e
sherwyn47 <rodriguessherwyn47@gmail.com>
2024-10-17 20:02:37 +0530

    Create Road cpp
    Create and declare read spline in cpp

---
+++ Source/DeathRaceCPP/Road.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Road.h"
+#include "Components/SplineComponent.h"
+
+// Sets default values
+ARoad::ARoad()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RoadSpline = CreateDefaultSubobject<USplineComponent>(TEXT("RoadSpline"));
+	RootComponent = RoadSpline; //Not Sure Check this out if its supposed to be root
+
+}
+
+// Called when the game starts or when spawned
+void ARoad::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARoad::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
